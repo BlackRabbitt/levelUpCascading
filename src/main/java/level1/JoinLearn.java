f34f891a27ab00e97b853eea8782772b630215c3
Sujit Shakya <sujishakya@system.deerwalk.com>
2014-03-07 14:26:12 +0545

    level1 equivalent to SQL Join has been written.

---
+++ src/main/java/level1/JoinLearn.java
@@ -0,0 +1,34 @@
+package level1;
+
+import cascading.flow.FlowDef;
+import cascading.pipe.CoGroup;
+import cascading.pipe.Pipe;
+import cascading.pipe.assembly.Rename;
+import cascading.pipe.joiner.LeftJoin;
+import cascading.tap.Tap;
+import cascading.tuple.Fields;
+
+/**
+ * Created by sujit shakya on 3/7/14.
+ */
+public class JoinLearn {
+    public static FlowDef JoinFiles(Tap source1Tap, Tap source2Tap, Tap sinkTap) {
+        // create a left pipe and right pipe before joining.
+        Pipe leftPipe = new Pipe("leftPipe");
+        Pipe rightPipe = new Pipe("rightPipe");
+        rightPipe = new Rename(rightPipe, new Fields("id"), new Fields("s_id"));
+
+        // output pipe after join
+        Pipe jointPipe ;
+
+        // In this level you do leftJoin but there are other option like rightJoin and InnerJoin just like in SQLJoin, (If you are familier with).
+        // You can always refer to userguide [cascading.org] for further details and information
+        jointPipe = new CoGroup(leftPipe, new Fields("id"), rightPipe, new Fields("s_id"),new LeftJoin());
+        return FlowDef.flowDef()
+                .addSource(leftPipe, source1Tap)
+                .addSource(rightPipe, source2Tap)
+                .addTail(jointPipe)
+                .addSink(jointPipe, sinkTap);
+
+    }
+}
